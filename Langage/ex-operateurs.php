0143dbe44b031db7b893b6a91349c7f94af94c7b
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-09-24 17:06:17 +0200

    Jour 1

--- Langage/ex-operateurs.php
+++ Langage/ex-operateurs.php
@@ -2,12 +2,14 @@
 
 // 1 - Créer 2 variables qui contiennent
 // votre prénom et votre nom :
-
+$prenom = 'Romain';
+$nom = 'Bohdanowicz';
 
 // 2 - Créer une variable qui contient
 // votre nom complet à partir des
 // variables prénom et nom :
-
+$nomComplet = $prenom . ' ' . $nom;
+$nomComplet = "$prenom $nom";
 
 // 3 - La variable suivant contient un
 // nom aléatoire compris en 0 et 100 :
@@ -15,16 +17,37 @@ $nbAlea = mt_rand(0, 100);
 
 // 4 - Afficher cette variable avec la fonction
 // var_dump :
+var_dump($nbAlea);
 
 // 5 - Créer une variable de type booléen
 // qui contient true si le nombre aléatoire
 // est supérieur à 10, false sinon
+$estSup10 = $nbAlea > 10;
 
 // 6 - Créer une variable qui contient true
 // si le nombre aléatoire est un multiple de 10, false sinon
+$estMultiple10 = $nbAlea % 10 === 0;
 
 // 7 - Intervertir le contenu de ces 2 variables
 // sans toucher au code existant :
 $nb1 = 1;
 $nb2 = 2;
 
+$temp = $nb1;
+$nb1 = $nb2;
+$nb2 = $temp;
+
+
+echo "\n";
+
+
+// Optionnel
+
+// 8 - Créer un tableau avec 10 nombre aléatoires entre 0 et 100
+
+// 9 - Afficher le nombre de valeurs paires et impairs
+
+// 10 - Faire la somme de tous les éléments du tableau 
+
+// 11 - Créer une variable $nbAleaPair qui contient
+// un nombre aléatoire pair
