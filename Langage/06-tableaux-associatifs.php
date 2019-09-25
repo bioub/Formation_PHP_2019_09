5726d47c4b44ffe8cd2f71e7bc2f063193b1d6a1
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-09-25 14:39:13 +0200

    Tableaux associatifs

---
+++ Langage/06-tableaux-associatifs.php
@@ -0,0 +1,22 @@
+<?php
+//$capitales = ['Paris', 'Berlin', 'Madrid'];
+
+$capitales = [
+    'France' => 'Paris',
+    'Allemagne' => 'Berlin', 
+    'Espagne' => 'Madrid'
+];
+
+echo "La capitale de la France est " . $capitales['France'] . "\n";
+echo "La capitale de la France est $capitales[France]\n";
+
+$user = [
+    'prenom' => 'Romain',
+    'nom' => 'Bohdanowicz',
+];
+
+$nomComplet = $user['prenom'] . ' ' . $user['nom'] . "\n";
+
+foreach ($capitales as $pays => $ville) {
+    echo "La capitale de $pays est $ville";
+}
